baf3ba071b7de1b186999e1e8fd4c1bcabb65fc6
Gwenael-3iL <117454901+Gwenael-3iL@users.noreply.github.com>
2022-11-07 10:59:04 +0000

    Exo point

--- Point.java
+++ Point.java
@@ -10,16 +10,36 @@ public class Point {
     System.out.println("Coordonnées " + x + " " + y);
   }
 
-  public static String normeMax(Point a, Point b) {
-    return "";
+  public static Point normeMax(Point a, Point b) {
+    int distAO = Math.sqrt((a.x)^2 + (a.y)^2);
+    int distBO = Math.sqrt((b.x)^2 + (b.y)^2);
+
+    if (distAO > distBO) {
+      result = a;
+    } else {
+      result = b
+    }
+    
+    return result;
   }
 
-  public String normeMax(Point b) {
-    return "";
+  public Point normeMax(Point b) {
+    Point result;
+    int distAO = Math.sqrt((this.x)^2 + (this.y)^2);
+    int distBO = Math.sqrt((b.x)^2 + (b.y)^2);
+
+    if (distAO > distBO) {
+      result = this;
+    } else {
+      result = b
+    }
+    
+    return result;
   }
 
   public void homothetie(double valeur) {
-    
+    this.x *= valeur;
+    this.y *= valeur;
   }
 
   public void rotation(int angle) {
